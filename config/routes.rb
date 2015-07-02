73275a1fa4f66c68166cb4cf99b17703d0cc193b
Mister-Ed <Mister-Ed@users.noreply.github.com>
2015-07-02 00:53:01 -0600

    few changes + readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root :to => 'contacts#index'
+
   get '/contacts' => 'contacts#index'
   get 'contacts/new' => 'contacts#new'
   post 'contacts' => 'contacts#create'
