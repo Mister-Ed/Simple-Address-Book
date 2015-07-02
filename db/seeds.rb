e71591857fb1eaad9b3b72d3c80562ff472c6a05
Mister-Ed <Mister-Ed@users.noreply.github.com>
2015-07-02 00:43:43 -0600

    restart dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-c1 = Contact.create(email: "Mister.Ed@AgoraCart.com", name: "Mister Ed", address: "Somewhere south of here, Provo, UT 84605", phone: "801-361-2421")
+c1 = Contact.create(email: "horsey@mister-ed.show", name: "Mister Ed", address: "Somewhere south of here, Provo, UT 84605", phone: "801-000-0001")
 c2 = Contact.create(email: "Super.Man@crushingonloislane.com", name: "Clark", address: "Ice Fortress Way, North Pole, AK 98999", phone: "unlisted number")
 c3 = Contact.create(email: "r2d2@starwars.com", name: "R2-D2 Astro Mech Driod", address: "Galaxy Far Away, Naboo, GX 1111 1111", phone: "1001 1101 0011")
 c4 = Contact.create(email: "yoda@greenshortguy.com", name: "Yoda", address: "there is no try lane, Jedi Temple, TX 50005", phone: "123-123-1234")
